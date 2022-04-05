56cb064db55ab4623f946f225a0411307d3eb8b1
Jean <billaudjean@gmail.com>
2022-04-05 18:28:59 +0200

    green(score): score on 2 frames

--- kata/src/solution.php
+++ kata/src/solution.php
@@ -17,6 +17,10 @@ class Solution
             return 0;
         }
 
+        if (!str_contains($frame, '-')) {
+            return (int) $frame[0] + (int) $frame[1];
+        }
+
         if ($frame[0] === '-') {
             return (int) $frame[1];
         }
